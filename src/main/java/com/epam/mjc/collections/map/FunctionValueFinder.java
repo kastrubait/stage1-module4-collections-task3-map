4365fcd1e8f964fc928b354d96e2c63f5351eaac
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-13 15:06:08 +0300

    feat: complete KeyValueSwapper module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
+++ src/main/java/com/epam/mjc/collections/map/FunctionValueFinder.java
@@ -1,13 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class FunctionValueFinder {
     public boolean isFunctionValuePresent(List<Integer> sourceList, int requiredValue) {
         Map<Integer, Integer> functionMap = calculateFunctionMap(sourceList);
+
+        return true;
     }
 
     private Map<Integer, Integer> calculateFunctionMap(List<Integer> sourceList) {
+
+        return new HashMap<>();
     }
 }
--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,9 +1,13 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
 
+        Map<String, Integer> word = new HashMap<>();
+
+        return word;
     }
 }
